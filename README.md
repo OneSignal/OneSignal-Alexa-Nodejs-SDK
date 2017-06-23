353f4c6502772a7532880006c0507aa794dafbc7
Josh Kasten <Josh@onesignal.com>
2017-06-23 16:15:40 -0700

    0.9.0 Release commit

---
+++ README.md
@@ -0,0 +1,17 @@
+OneSignal Alexa Push Notification Plugin for Node.js
+====================================
+
+OneSignal is a free push notification service for mobile apps and websites. This plugin makes it easy to integrate your Amazon Alexa Skill with OneSignal.
+
+#### Installation
+Coming soon
+
+#### API
+Coming soon
+
+#### Change Log
+See this repository's [release tags](https://github.com/OneSignal/OneSignal-Alexa-Nodejs-SDK/releases) for a complete change log of every released version.
+
+#### Support
+Please visit this repository's [Github issue tracker](https://github.com/OneSignal/OneSignal-Alexa-Nodejs-SDK/issues) for feature requests and bug reports related specificly to the SDK.
+For account issues and support please contact OneSignal support from the [OneSignal.com](https://onesignal.com) dashboard.
---
+++ index.js
@@ -0,0 +1,265 @@
+/**
+ * Modified MIT License
+ * 
+ * Copyright 2017 OneSignal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * 1. The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * 2. All copies of substantial portions of the Software may only be used in connection
+ * with services provided by OneSignal.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+var OneSignal = {
+  _VERSION: "000900",
+  
+  _API_HOST: "onesignal.com",
+  
+  _API_BASE_PATH: "/api/v1/",
+  _API_DEFAULT_HEADERS: {
+    "Content-Type": "application/json; charset=utf-8",
+  },
+  
+  // attributes that are saved and read from the DB.
+  _scope_attributes: null,
+  
+  _appId: null,
+  _userId: null,
+  
+  _pendingTags: null,
+  
+  init: function (app_id, alexa, event) {
+    OneSignal._appId = app_id;
+    alexa.registerHandlers(OneSignal._alexaHandlers);
+  },
+  
+  // Alexa Skill event handlers
+  _alexaHandlers: {
+   // Messaging.MessageReceived':
+    'Unhandled': function() {
+      OneSignal._stateSetup(this);
+      OneSignal._processMessageReceived(this.event);
+    },
+    'LaunchRequest': function () {
+      OneSignal._stateSetup(this);
+      OneSignal._newSession(this);
+    }
+  },
+  
+  _stateSetup: function(mainScope) {
+    OneSignal._scope_attributes = mainScope.attributes['onesignal_sdk'];
+    
+    if (typeof OneSignal._scope_attributes == 'undefined')
+      OneSignal._scope_attributes = {};
+    else {
+      if (OneSignal._scope_attributes.userId !== undefined)
+        OneSignal._userId = OneSignal._scope_attributes.userId;
+    }
+  },
+  
+  _userPut: function(payload, error_string) {
+    var options = {
+      host: OneSignal._API_HOST,
+      path: OneSignal._API_BASE_PATH + "players/" + OneSignal._userId,
+      method: "PUT",
+      headers: OneSignal._API_DEFAULT_HEADERS
+    };
+    
+    var https = require('https');
+    var req = https.request(options, function(res) {
+      res.on('data', function(data) {
+      });
+    });
+    
+    req.on('error', function(e) {
+      console.log("OneSignal - " + error_string + " ERROR:");
+      console.log(e);
+    });
+    
+    req.write(JSON.stringify(payload));
+    req.end();
+  },
+  
+  sendTags: function(tags) {
+    if (OneSignal._userId == null) {
+       if (OneSignal._pendingTags == null)
+         OneSignal._pendingTags = {};
+       Object.assign(OneSignal._pendingTags, tags);
+       return;
+    }
+    
+    OneSignal._pendingTags = null;
+    OneSignal._userPut({tags: tags}, "sendTags");
+  },
+  
+  _registerDevice: function(data, mainScope, callback) {
+    var url_path = OneSignal._API_BASE_PATH + "players";
+    
+    if (OneSignal._userId != null)
+       url_path += "/" + OneSignal._userId + "/on_session";
+    
+    var options = {
+      host: OneSignal._API_HOST,
+      path: url_path,
+      method: "POST",
+      headers: OneSignal._API_DEFAULT_HEADERS
+    };
+    
+    if (OneSignal._pendingTags != null) {
+      data.tags = OneSignal._pendingTags;
+      OneSignal._pendingTags = null;
+    }
+    
+    var https = require('https');
+    var req = https.request(options, function(res) {
+      res.on('data', function(data) {
+        if (typeof JSON.parse(data).id != 'undefined') {
+           OneSignal._userId = JSON.parse(data).id;
+           
+           if (OneSignal._pendingTags == null)
+             OneSignal.sendTags(OneSignal._pendingTags);
+           
+           OneSignal._scope_attributes.userId = OneSignal._userId;
+           mainScope.attributes['onesignal_sdk'] = OneSignal._scope_attributes;
+           mainScope.emit(':saveState', true);
+        }
+      });
+    });
+    
+    req.on('error', function(e) {
+      console.log("OneSignal - Register device ERROR:");
+      console.log(e);
+    });
+    
+    req.write(JSON.stringify(data));
+    req.end();
+  },
+  
+  _newSession: function(mainScope) {
+    var event = mainScope.event;
+    if (!event.session.new)
+      return;
+    
+    var devicePayload = {
+       app_id: OneSignal._appId,
+       device_type: 10,
+       sdk: OneSignal._VERSION,
+       notification_types: OneSignal.hasNotificationPermissions(event) ? 1 : 0,
+       identifier: event.context.System.user.userId
+     };
+     
+     OneSignal._registerDevice(devicePayload, mainScope);
+  },
+  
+  promptForNotificationPermissions: function(mainScope) {
+    mainScope.handler.response = {
+      "version": "1.0",
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "Please open the Alexa App and accept the notification permissin card."
+        },
+        "card": {
+          "type": "AskForPermissionsConsent",
+          "permissions": ["write::alexa:devices:all:notifications:standard"]
+        }
+      }
+    };
+    mainScope.emit(':responseReady');
+  },
+  
+  hasNotificationPermissions : function(event) {
+     var permissions = event.context.System.user.permissions;
+     return typeof permissions != 'undefined' && typeof permissions.consentToken != 'undefined';
+  },
+  
+  _processMessageReceived: function(event) {
+     if (event.request.type != 'Messaging.MessageReceived')
+        return;
+     
+     if (!OneSignal.hasNotificationPermissions(event)) {
+       if (OneSignal._userId != null)
+         OneSignal._userPut({notification_types: 0}, "unsubscribing");
+       return;
+     }
+      
+      var expiryTime = new Date();
+      
+      if (event.request.message.ttl === undefined)
+        expiryTime.setHours(expiryTime.getHours() + 24);
+      else {
+        // Ensure we are not going over the 24 hour max limit.
+        var secOffset = event.request.message.ttl;
+        if (secOffset> 86400)
+          secOffset = 86400;
+        
+        expiryTime.setSeconds(expiryTime.getSeconds() + secOffset);
+      }
+      
+      var display_title = event.request.message.display_title;
+      if (typeof display_title == 'undefined')
+        display_title = event.request.message.spoken_text;
+      
+      OneSignal._createNotification(event.context.System.user.permissions.consentToken, {
+        "expiryTime": expiryTime.toISOString(),
+        "referenceId": event.request.message.custom.i,
+        "spokenInfo": {
+          "content":[{
+             "locale": "en-US",
+             "text": event.request.message.spoken_text
+  //         "ssml": "<speak>Overrides text</speak>"
+          }]
+        },
+        displayInfo:{
+          content:[{
+            locale: "en-US",
+            toast:{ primaryText: event.request.message.spoken_text },
+            title: display_title,
+            bodyItems:[{ primaryText: event.request.message.spoken_text}]
+         }]
+        }
+      });
+  },
+  
+  _createNotification: function(token, data) {
+    var options = {
+      host: "api.amazonalexa.com",
+      path: "/v2/notifications",
+      method: "POST",
+      headers: Object.assign({"Authorization": "Bearer " + token},
+                             OneSignal._API_DEFAULT_HEADERS)
+    };
+    
+    var https = require('https');
+    var req = https.request(options, function(res) {
+      res.on('data', function(data) {
+      });
+    });
+    
+    req.on('error', function(e) {
+      console.log("OneSignal - Error sending create notification:");
+      console.log(e);
+    });
+    
+    req.write(JSON.stringify(data));
+    req.end();
+  }
+};
+
+module.exports = OneSignal;
